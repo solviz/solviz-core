c550e286cae8839de352574e5adab8f782d0f871
Bernardo Vieira <bernardo5@live.com.pt>
2019-04-06 13:23:28 +0100

    start a custom token to test more use cases

---
+++ test/contracts/MyNFT.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+
+contract MyNFT is ERC721Full, ERC721Mintable {
+    constructor() ERC721Full("MyNFT", "MNFT") public {
+    }
+}
