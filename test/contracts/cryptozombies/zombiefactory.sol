f8314e765b912f1bc8bf6ec42648545391c92fe4
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-24 22:36:20 +0100

    updates contracts for tests

--- test/contracts/cryptozombies/zombiefactory.sol
+++ test/contracts/cryptozombies/zombiefactory.sol
@@ -27,19 +27,19 @@ contract ZombieFactory is Ownable {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) internal {
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0, "");
         uint randDna = _generateRandomDna(_name);
         randDna = randDna - randDna % 100;
